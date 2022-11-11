efd5ba724d1197496732d013b436c017fddcedfc
FluffyWaffles2022 <113785692+JacobSapecky@users.noreply.github.com>
2022-11-10 22:39:55 -0500

    Functionality Complete
    Functionality Complete. next steps is to finish up the CSS, test, and clean up.

--- src/Components/EmployeeCards.js
+++ src/Components/EmployeeCards.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card';
 import {Link} from 'react-router-dom'
+import { formatter } from '../Data/functions';
 
 export default function EmployeeCards(props) { //the props parameter is how the variables passed from the parent, MainPage.js, is accessed
 
@@ -9,15 +10,15 @@ export default function EmployeeCards(props) { //the props parameter is how the 
     <>
     <Card className="cards">
       <Card.Body>
-        <Card.Title><Link to='/invoice' state={{id: props.id, name: props.name}}>{props.name}</Link></Card.Title>
+        <Card.Title><Link to='/invoice' state={{key: props.id, id: props.id, name: props.name}}>{props.name}</Link></Card.Title>
         <Card.Text>
           <p>🎂Birthdate: {props.birthdate} </p>
           <p>🧁Days until birthday: {props.daysUntilBirthday} </p>
           <p>📧Email: <a href={"mailto:" + props.email}>{props.email}</a></p>
-          <p>💵2012 Revenue: ${props.twentytwelve}</p>
-          <p>💵2013 Revenue: ${props.twentythirteen}</p>
+          <p>💵2012 Revenue: {formatter.format(props.twentytwelve)}</p>
+          <p>💵2013 Revenue: {formatter.format(props.twentythirteen)}</p>
           <p>🧑Best Customer: {props.bestCustomer} </p>
-          <p>💰Commission: ${props.commissionmet}</p>
+          <p>💰Commission: {formatter.format(props.commissionmet)}</p>
         </Card.Text>
       </Card.Body>
     </Card>
